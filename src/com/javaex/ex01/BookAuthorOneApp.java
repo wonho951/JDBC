bd2c42d10bcc1a9952393c19907c249e424f4cb0
wonho <wonho951@gmail.com>
2021-07-01 16:41:41 +0900

    2021.07.01 수업내용(jdbc 실습)

---
+++ src/com/javaex/ex01/BookAuthorOneApp.java
@@ -0,0 +1,88 @@
+package com.javaex.ex01;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class BookAuthorOneApp {
+
+	public static void main(String[] args) {
+		
+		//book,author테이블 조인한거 출력
+		
+		// 0. import java.sql.*;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		
+		
+		/*작가테이블*/
+		try {
+		    // 1. JDBC 드라이버 (Oracle) 로딩
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+		    // 2. Connection 얻어오기
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			conn = DriverManager.getConnection(url, "webdb", "webdb");
+
+		    // 3. SQL문 준비 / 바인딩 / 실행
+			String query = "";
+			query += " select  b.book_id, ";
+			query += " 		   b.title, ";
+			query += "         b.pubs, ";
+			query += "         b.pub_date, ";
+			query += "         a.author_name, ";
+			query += "         a.author_desc ";
+			query += " from book b, author a ";
+			query += " where b.book_id = a.author_id ";
+			query += " and b.book_id = ? ";
+			
+			pstmt = conn.prepareStatement(query);
+			pstmt.setInt(1, 2);
+			
+			
+			rs = pstmt.executeQuery();
+			
+			
+			
+		    // 4.결과처리
+
+			while (rs.next()) {	
+				int bookId = rs.getInt("book_id");
+				String title = rs.getString("title");
+				String pubs = rs.getString("pubs");
+				String pubDate = rs.getString("pub_date");
+				String authorName = rs.getString("author_name");
+				String authorDesc = rs.getString("author_desc");
+				
+				System.out.println(bookId + ", " + title + ", " + pubs + ", " + pubDate + ", " + authorName + ", " + authorDesc);
+			}
+		} catch (ClassNotFoundException e) {
+		    System.out.println("error: 드라이버 로딩 실패 - " + e);
+		} catch (SQLException e) {
+		    System.out.println("error:" + e);
+		} finally {
+		   
+		    // 5. 자원정리
+		    try {
+		        if (rs != null) {
+		            rs.close();
+		        }                
+		        if (pstmt != null) {
+		            pstmt.close();
+		        }
+		        if (conn != null) {
+		            conn.close();
+		        }
+		    } catch (SQLException e) {
+		        System.out.println("error:" + e);
+		    }
+
+		}
+
+	}
+
+}
---
+++ src/com/javaex/ex01/BookSelectApp.java
@@ -0,0 +1,84 @@
+package com.javaex.ex01;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class BookSelectApp {
+
+	public static void main(String[] args) {
+		
+		//book테이블 정보 출력 --> 시간은 어떻게 처리함?
+		
+		
+		// 0. import java.sql.*;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+		    // 1. JDBC 드라이버 (Oracle) 로딩
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+		    // 2. Connection 얻어오기
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			conn = DriverManager.getConnection(url, "webdb", "webdb");
+
+		    // 3. SQL문 준비 / 바인딩 / 실행
+			String query = "";
+			query += " select  book_id, ";
+			query += "		   title, ";
+			query += "         pubs, ";
+			query += "         pub_date, ";
+			query += "         author_id ";
+			query += " from book ";
+			
+			
+			pstmt = conn.prepareStatement(query);
+			rs = pstmt.executeQuery();
+			
+			
+		    // 4.결과처리
+
+			while (rs.next()) {	
+				int bookId = rs.getInt("book_id");
+				String title = rs.getString("title");
+				String pubs = rs.getString("pubs");
+				String pubDate = rs.getString("pub_date");
+				String authorId = rs.getString("author_id");
+				
+				System.out.println(bookId + ", " + title + ", " + pubs + ", " + pubDate + ", " + authorId);
+			}
+			
+			
+			
+			
+		} catch (ClassNotFoundException e) {
+		    System.out.println("error: 드라이버 로딩 실패 - " + e);
+		} catch (SQLException e) {
+		    System.out.println("error:" + e);
+		} finally {
+		   
+		    // 5. 자원정리
+		    try {
+		        if (rs != null) {
+		            rs.close();
+		        }                
+		        if (pstmt != null) {
+		            pstmt.close();
+		        }
+		        if (conn != null) {
+		            conn.close();
+		        }
+		    } catch (SQLException e) {
+		        System.out.println("error:" + e);
+		    }
+
+		}
+
+
+	}
+
+}
