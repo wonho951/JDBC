ca61121ab0a94160d6bf8af696e9ae1efe0c291e
wonho <wonho951@gmail.com>
2021-07-02 11:12:37 +0900

    2021.07.02 수업내용(DAO)

---
+++ src/com/javaex/ex02/AuthorApp.java
@@ -0,0 +1,60 @@
+package com.javaex.ex02;
+
+public class AuthorApp {
+
+	public static void main(String[] args) {
+		
+		AuthorDao authorDao = new AuthorDao();
+		
+		
+		//리스트출력
+		List<> authorList = authorDao.getAuthorList();
+		//리스트를 for문으로 출력
+		
+		
+		
+		
+		
+		
+		
+		/*
+		//작가 등록
+		authorDa.authorInsert("김일성", "뽀글머리");
+		
+		
+		
+		//리스트출력
+		List<> authorList = authorDao.getAuthorList();
+		//리스트를 for문으로 출력
+		
+		
+		//작가 수정
+		authorDao.authorUpdate(3, "김정은", "돼지");
+		
+		
+		
+		//리스트출력
+		List<> authorList = authorDao.getAuthorList();
+		//리스트를 for문으로 출력
+		
+		
+		//작가 삭제
+		authorDao.authorDelete(3);
+		
+		
+		
+		//리스트출력
+		List<> authorList = authorDao.getAuthorList();
+		//리스트를 for문으로 출력
+		
+		
+		//작가 한명의 정보
+		??? = authorDao.getAuthorOne(3);	//3번 작가정보 출력
+		*/
+		
+		
+		
+		
+	}
+
+}
---
+++ src/com/javaex/ex02/AuthorDao.java
@@ -0,0 +1,35 @@
+package com.javaex.ex02;
+
+public class AuthorDao {
+
+	//필드
+	
+	//생성자
+	
+	//메소드g/s
+	
+	//메소드 - 일반
+	public void getAuthorList(){
+		//DB값을 가져와서 ArrayList로 전달
+		
+	}
+	
+	getAuthorOne(){
+		
+	}
+	
+	
+	authorInsert(){
+		
+	}
+	
+	authorUpdate(){
+		
+	}
+	
+	authorDelete(){
+		
+	}
+	
+	
+}
