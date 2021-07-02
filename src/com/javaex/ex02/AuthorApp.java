b5bb904a492a75ee5bd8997f934859e7d62952e9
wonho <wonho951@gmail.com>
2021-07-02 13:07:15 +0900

    2021.07.02 수업내용(DAO - 출력, 등록)

--- src/com/javaex/ex02/AuthorApp.java
+++ src/com/javaex/ex02/AuthorApp.java
@@ -7,17 +7,42 @@ public class AuthorApp {
 	public static void main(String[] args) {
 		
 		AuthorDao authorDao = new AuthorDao();
-		
+		List<AuthorVo> authorList;
 		
 		//리스트출력
-		List<AuthorVo> authorList = authorDao.getAuthorList();
+		//DB에서 데이터 가져오기
+		authorList = authorDao.getAuthorList();
 		//리스트를 for문으로 출력
 		for(int i = 0; i < authorList.size(); i++) {
+			
+			/*
+			 * authorList.get(i) 의 반복이기 때문에 다른 표현법
 			int authorId = authorList.get(i).getAuthorId();
 			String authorName = authorList.get(i).getAuthorName();
 			String authorDesc = authorList.get(i).getAuthorDesc();
 			
 			System.out.println(authorId + "\t" + authorName + "\t\t" + authorDesc);
+			*/
+			
+			
+			// authorList.get(i)의 반복이기 때문에 authorVo에 authorList.get(i)를 담아줌
+			/*
+			AuthorVo authorVo = authorList.get(i);
+			
+			int authorId = authorVo.getAuthorId();
+			String authorName = authorVo.getAuthorName();
+			String authorDesc = authorVo.getAuthorDesc();
+			
+			System.out.println(authorId + "\t" + authorName + "\t\t" + authorDesc);
+			*/
+			
+			//authorId같은 변수를 필요없게 하는 방법
+			
+			AuthorVo authorVo = authorList.get(i);
+			System.out.println(authorVo.getAuthorId() + "\t" + authorVo.getAuthorName() + "\t" + authorVo.getAuthorDesc());
+			
+			
+			
 		}
 		
 		
@@ -25,17 +50,29 @@ public class AuthorApp {
 		
 		
 		
-		/*
 		//작가 등록
-		authorDa.authorInsert("김일성", "뽀글머리");
+		int count = authorDao.authorInsert("김일성", "뽀글머리");
 		
-		
+		if(count > 0) {	
+		System.out.println("[등록되었습니다.]");
+		} else {
+			System.out.println("[관리자에게 문의하세요(" + count + ")]");
+		}
 		
 		//리스트출력
-		List<> authorList = authorDao.getAuthorList();
+		//DB에서 데이터 가져오기
+		authorList = authorDao.getAuthorList();
+		
 		//리스트를 for문으로 출력
+		for(int i = 0; i < authorList.size(); i++) {
+						
+			AuthorVo authorVo = authorList.get(i);
+			System.out.println(authorVo.getAuthorId() + "\t" + authorVo.getAuthorName() + "\t" + authorVo.getAuthorDesc());
+						
+		}
 		
 		
+		/*
 		//작가 수정
 		authorDao.authorUpdate(3, "김정은", "돼지");
 		
--- src/com/javaex/ex02/AuthorDao.java
+++ src/com/javaex/ex02/AuthorDao.java
@@ -17,6 +17,8 @@ public class AuthorDao {
 	//메소드g/s
 	
 	//메소드 - 일반
+	
+	//작가리스트 가져오기
 	public List<AuthorVo> getAuthorList(){
 		//DB값을 가져와서 ArrayList로 전달
 		
@@ -89,16 +91,71 @@ public class AuthorDao {
 		return authorList;
 		
 	}
+	
+	//getAuthorOne(){
+		
+//	}
+	
+	
+	//작가 등록하기
+	public int authorInsert(String authorName, String authorDesc){
+		
+		int count = 0;
+		
+		
+		// 0. import java.sql.*;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+		    // 1. JDBC 드라이버 (Oracle) 로딩
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+		    // 2. Connection 얻어오기
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			conn = DriverManager.getConnection(url, "webdb", "webdb");
+
+		    // 3. SQL문 준비 / 바인딩 / 실행
+		    String query = "";
+		    query +=" insert into author ";
+		    query +=" values(seq_author_id.nextval, ?, ?) ";
+		    
+		    pstmt = conn.prepareStatement(query);
+		    pstmt.setString(1, authorName);
+		    pstmt.setString(2, authorDesc);
+		    
+		    
+		    count = pstmt.executeUpdate();
+		    // 4.결과처리
+		    System.out.println(count + "건이 등록되었습니다.");	//등록할땐 개발자가 보는 용도로만.
+		    
+		} catch (ClassNotFoundException e) {
+		    System.out.println("error: 드라이버 로딩 실패 - " + e);
+		} catch (SQLException e) {
+		    System.out.println("error:" + e);
+		} finally {
+		   
+		    // 5. 자원정리
+		    try {
+		        if (rs != null) {
+		            rs.close();
+		        }                
+		        if (pstmt != null) {
+		            pstmt.close();
+		        }
+		        if (conn != null) {
+		            conn.close();
+		        }
+		    } catch (SQLException e) {
+		        System.out.println("error:" + e);
+		    }
+
+		}
+
+		return count;	//성공갯수 리턴
+	}
 	/*
-	getAuthorOne(){
-		
-	}
-	
-	
-	authorInsert(){
-		
-	}
-	
 	authorUpdate(){
 		
 	}
